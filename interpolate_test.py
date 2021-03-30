dd0abd79305a4df07d2e0bc6b84b0a3b26d8691b
John Welsh <jaybdub@users.noreply.github.com>
2021-03-30 12:04:43 -0700

    added inteprolate test

--- interpolate_test.py
+++ interpolate_test.py
@@ -1,6 +1,7 @@
 import torch
 from torch2trt import torch2trt
-
+from torchvision.transforms import ToTensor, ToPILImage
+from PIL import Image
 
 data = torch.randn(1, 3, 256, 256).cuda()
 
@@ -19,8 +20,19 @@ model = Interpolate().cuda()
 print('Optimizing with torch2trt...')
 model_trt = torch2trt(model, [data], fp16_mode=True)
 
-data = torch.randn(1, 3, 256, 256).cuda()
+data = Image.open('grocery_256.jpg')
+data = ToTensor()(data).cuda()[None, ...]
 
-err = torch.max(torch.abs(model(data) - model_trt(data)))
+out_torch = model(data)
+out_trt = model_trt(data)
+
+image_torch = ToPILImage()(out_torch.cpu().detach()[0])
+image_trt = ToPILImage()(out_trt.cpu().detach()[0])
+
+image_torch.save('image_torch.jpg')
+image_trt.save('image_trt.jpg')
+
+err = torch.max(torch.abs(out_torch - out_trt))
+
 
 print(err)
